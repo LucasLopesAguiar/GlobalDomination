94b0cb90b1b8ba1c99531a8cc63a1f3be4152384
LeonanML <leonan.ml97@gmail.com>
2018-09-13 22:02:33 -0300

    Modificação do AppConfig(ConnectionStreings); Atualização do paco EN para todo o projeto;

--- BibliotecaDigital/Modelos/DAL/Contexto.cs
+++ BibliotecaDigital/Modelos/DAL/Contexto.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Modelos.DAL
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
-        
+        public Contexto() : base ("conn")
+        {
+            
+        }
     }
 }
